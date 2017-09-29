8eefd84287e0d1e9ca3ffd9bcfc1c93c90144124
yuliming <yuliming@phc-dow.com>
2017-09-29 16:02:11 +0800

    Initial commit

---
+++ src/main/java/com/dawn/spring/domain/User.java
@@ -0,0 +1,37 @@
+package com.dawn.spring.domain;
+
+/**
+ * 用户类
+ *
+ * @author yuliming
+ * @create 2017-09-29 15:42
+ */
+public class User {
+    private Integer id;
+    private String name;
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
---
+++ src/main/java/com/dawn/spring/swaggerDemoApplication.java
@@ -0,0 +1,20 @@
+package com.dawn.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目启动类
+ *
+ * @author yuliming
+ * @create 2017-09-29 14:48
+ */
+@SpringBootApplication
+public class swaggerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(swaggerDemoApplication.class, args);
+    }
+
+
+}
